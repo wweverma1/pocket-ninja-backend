7e2e3666b6e775021a2d84a7629a206a7ce20beb
wweverma1 <wweverma1@gmail.com>
2025-12-19 02:18:05 +0900

    updated method for updating username to PUT and allowing CORS

--- app/user/routes.py
+++ app/user/routes.py
@@ -8,5 +8,5 @@ from app.user.controller import (
 user_endpoints = Blueprint('user', __name__, url_prefix="/user")
 
 user_endpoints.add_url_rule(rule='/', view_func=get_profile, methods=['GET'])
-user_endpoints.add_url_rule(rule='/', view_func=update_username, methods=['POST'])
+user_endpoints.add_url_rule(rule='/', view_func=update_username, methods=['PUT'])
 
--- app/utils/app_functions.py
+++ app/utils/app_functions.py
@@ -1,21 +1,34 @@
 import time
 
 from datetime import datetime
-from flask import request
+from flask import request, make_response
 from app import app
 
 
 @app.before_request
 def before_request():
+    # 1. Handle Preflight OPTIONS requests immediately
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        return response, 200
+
     request.start_time = time.time()
 
 
 @app.after_request
 def after_request(response):
-    if request.endpoint:
+    # 2. Add CORS headers to all successful requests
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS'
+
+    # 3. Logging (only if it wasn't an OPTIONS request)
+    if request.method != 'OPTIONS' and request.endpoint:
         end_time = time.time()
         latency = int((end_time - request.start_time) * 1000)
-        header = response.headers
-        header['Access-Control-Allow-Origin'] = '*'
-        print(f'[ {str(datetime.now())} ] endpoint {request.endpoint} latency {latency} req_id {request.environ.get("FLASK_REQUEST_ID")}')
+        print(f'[ {str(datetime.now())} ] endpoint {request.endpoint} latency {latency}ms')
+        
     return response
